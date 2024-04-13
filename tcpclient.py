1adcb50e7b43a5eb49fea725e9fe7887b4ea0f99
NhanNguyen <nnguyen14@sfsu.edu>
2024-04-12 19:51:12 -0700

    add home IP address and change prompt

--- tcpclient.py
+++ tcpclient.py
@@ -1,15 +1,14 @@
 from socket import *
 
-serverName = '10.143.15.196'
+# serverName = '10.143.15.196'  # school IP address
+serverName = '192.168.56.1'  # home IP address
+
 serverPort = 12000
-# clientSocket = socket(AF_INET, SOCK_STREAM)
-# clientSocket.connect((serverName, serverPort))
-sentence = 'a'
-while sentence != 'q':
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
-    sentence = input('Input lowercase sentence:')
-    clientSocket.send(sentence.encode())
-    modifiedSentence = clientSocket.recv(1024)
-    print('From Server: ', modifiedSentence.decode())
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+sentence = input('Input a sentence you want to reverse: ')  # Get user input
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+print("Message was received and reversed to: " + modifiedSentence.decode())  # Print the reversed message received from the server
 clientSocket.close()
