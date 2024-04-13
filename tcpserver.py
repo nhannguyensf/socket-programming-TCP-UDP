250a189cd7f167e08be3abbec35ab126b3e88615
NhanNguyen <nnguyen14@sfsu.edu>
2024-04-12 19:49:23 -0700

    add function to reverse received message

--- tcpserver.py
+++ tcpserver.py
@@ -1,14 +1,19 @@
 from socket import *
 
+def reverse_sentence(sentence):
+    words = sentence.split()
+    reversed_sentence = ' '.join(reversed(words))
+    return reversed_sentence
+
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
-print('The server is ready to receive')
+print('The server is ready to receive!')
 while True:
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    print('New sentence: ' + capitalizedSentence)
-    connectionSocket.send(capitalizedSentence.encode())
+    message = connectionSocket.recv(1024).decode()
+    modifiedMessage = reverse_sentence(message)
+    print('Received "' + message + '" and reversed to: "' + modifiedMessage + '"')
+    connectionSocket.send(modifiedMessage.encode())
     connectionSocket.close()
