482d12c649d28d0de583ef24d8db754f965ee4ad
NhanNguyen <nnguyen14@sfsu.edu>
2024-04-12 17:43:51 -0700

    change IP address and add code comments

--- udpclient.py
+++ udpclient.py
@@ -1,14 +1,17 @@
 from socket import *
 
 # serverName = '10.143.217.160'
-serverName = '10.143.15.196'
+# serverName = '10.143.15.196'  # school IP address
+serverName = '192.168.56.1'  # home IP address
 
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = 'a'
-while message != 'q':
-    message = input('Input lowercase sentence:')
+message = None
+while message != '!q':  # Loop until the user types '!q'
+    message = input('Input lowercase sentence: ')  # Get user input
+    if message == '!q':
+        break  # Exit the loop before sending '!q' to the server
     clientSocket.sendto(message.encode(), (serverName, serverPort))
     modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-    print(modifiedMessage.decode())
+    print("Message was received and reversed to: " + modifiedMessage.decode())  # Print the reversed message received from the server
 clientSocket.close()
