6737ddbf04a9b28566a69403c5703405c6d4611c
NhanNguyen <nnguyen14@sfsu.edu>
2024-04-12 17:42:54 -0700

    radd function to everse  the message

--- udpserver.py
+++ udpserver.py
@@ -1,11 +1,17 @@
 from socket import *
 
+def reverse_sentence(sentence):
+    words = sentence.split()
+    reversed_sentence = ' '.join(reversed(words))
+    return reversed_sentence
+
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
-print("The server is ready to receive")
+print("The server is ready to receive!")
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    print('Received message and modified to: ' + modifiedMessage)
+    message = message.decode()  # Decode received message
+    modifiedMessage = reverse_sentence(message)  # Reverse sentence
+    print('Received "' + message + '" and reversed to: "' + modifiedMessage + '"')
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
